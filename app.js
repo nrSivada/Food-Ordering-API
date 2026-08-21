6b6f270126c23b87bf03f2916036e457a8fef347
Your Name <sivadanrajeev19@gmail.com>
2026-08-21 10:56:43 +0530

    server setting

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.get('/',(req, res)=>{
+    res.json({
+        success: true,
+        message: "Food ordering API is running."
+    });
+});
+
+
+module.exports = app;
