02064e49738377a38199cefd2873cc5a039df61b
Your Name <sivadanrajeev19@gmail.com>
2026-08-28 23:15:20 +0530

    created model for food

---
+++ models/Food.js
@@ -0,0 +1,41 @@
+const mongoose = require('monogoose');
+
+const foodSchema = new mongoose.Schema({
+    restaurant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Restaurant',
+        required: [true, "Restaurant is required."]
+    },
+    name: {
+        type: String,
+        required: [true, "Food name is required."],
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    category: {
+        type: String,
+        required: [true, "Category is required."],
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: [true, "Price is required."]
+    },
+    imageUrl: {
+        type: String,
+        trim: true
+    },
+    isAvailable: {
+        type: Boolean,
+        default:true
+    }
+},
+{
+    timestamps: true
+}
+);
+
+module.exports = mongoose.model('Food', foodSchema);
