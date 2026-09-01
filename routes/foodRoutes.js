5a8bd865921b1a63d0a88219a6e4edc33daba2f5
Your Name <sivadanrajeev19@gmail.com>
2026-09-01 14:57:26 +0530

    middleware

--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { addFood, getRestaurantFood, updateFood, deleteFood, getFoods } = require('../controllers/foodController');
+const authMiddleware = require('../middleware/authMiddleware');
+const adminMiddleware = require('../middleware/adminMiddleware');
 
 const router = express.Router();
 
-router.post('/restaurants/:restaurantId/foods', addFood);
+router.post('/restaurants/:restaurantId/foods', authMiddleware, adminMiddleware, addFood);
 
 router.get('/restaurants/:restaurantId/foods', getRestaurantFood);
 
--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const {createRestaurant, getRestaurant, getRestaurantById, updateRestaurant, deleteRestaurant} = require("../controllers/restaurantController");
+const authMiddleware = require('../middleware/authMiddleware');
+const adminMiddleware = require('../middleware/adminMiddleware');
 
-router.post('/', createRestaurant);
+router.post('/', authMiddleware, adminMiddleware, createRestaurant);
 
 router.get('/', getRestaurant);
 
